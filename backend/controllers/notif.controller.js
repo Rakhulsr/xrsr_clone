bbc59eb10bdc8fb5404af397c61670e8b4599214
Rakhulsr <shomarizky123@gmail.com>
2024-04-28 13:36:21 +0700

    4.1 - notification controller added pt.2

--- backend/controllers/notif.controller.js
+++ backend/controllers/notif.controller.js
@@ -29,3 +29,29 @@ export const deleteNotif = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const deleteOneNotif = async (req, res) => {
+  try {
+    const notifId = req.params.id;
+
+    const userId = req.user._id;
+
+    const notification = await Notification.findById(notifId);
+
+    if (!notification) {
+      res.status(404).json({ message: "Notif Not Found" });
+    }
+
+    if (notification.to.toString() != userId.toString()) {
+      return res
+        .status(403)
+        .json({ message: "you not have permissioan to delete notif" });
+    }
+
+    await Notification.findByIdAndDelete(notifId);
+    res.status(200).json({ message: "Notif Deleted" });
+  } catch (error) {
+    console.error("error in deleteOneNotif", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
--- backend/routes/notif.routes.js
+++ backend/routes/notif.routes.js
@@ -1,11 +1,16 @@
 import express from "express";
 
 import { protectedMe } from "../middleware/protectedMe.js";
-import { getNotif, deleteNotif } from "../controllers/notif.controller.js";
+import {
+  getNotif,
+  deleteNotif,
+  deleteOneNotif,
+} from "../controllers/notif.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectedMe, getNotif);
 router.delete("/", protectedMe, deleteNotif);
+router.delete("/:id", protectedMe, deleteOneNotif);
 
 export default router;
